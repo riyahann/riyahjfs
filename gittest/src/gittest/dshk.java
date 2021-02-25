2a149fb9fb19d38058c086393304d347d6b8bed3
Jude <Jude@DESKTOP-F2Q0FRO>
2021-02-25 22:01:03 +0530

    updation

---
+++ gittest/src/gittest/dshk.java
@@ -0,0 +1,24 @@
+package gittest;
+
+public class dshk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a [][]= {{4,5,6},{1,2,3,},{7,8,9}};
+        for (int i=0;i<3;i++)
+        {
+        for(int j=0;j<3;j++)
+        {
+        System.out.print(a[j][i]+" ");
+        }
+        System.out.println();
+        }
+        
+
+	
+
+
+
+	}
+
+}
